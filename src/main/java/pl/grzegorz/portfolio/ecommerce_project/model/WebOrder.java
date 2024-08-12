4f2382a283395fbd70737eff369258e296c2a33f
Grzegorz <grzegorz_sroda@wp.pl>
2024-08-12 14:19:34 +0200

    "Add JPA entities for Adress, Inventory, User, Product, WebOrder, OrderQuantity

---
+++ src/main/java/pl/grzegorz/portfolio/ecommerce_project/model/WebOrder.java
@@ -0,0 +1,59 @@
+package pl.grzegorz.portfolio.ecommerce_project.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "web_order")
+public class WebOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "local_user_id", nullable = false)
+    private LocalUser user;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "adress_id", nullable = false)
+    private Adress adress;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<WebOrderQuantity> quantities = new ArrayList<>();
+
+    public List<WebOrderQuantity> getQuantities() {
+        return quantities;
+    }
+
+    public void setQuantities(List<WebOrderQuantity> quantities) {
+        this.quantities = quantities;
+    }
+
+    public Adress getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+
+    public LocalUser getUser() {
+        return user;
+    }
+
+    public void setUser(LocalUser user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
