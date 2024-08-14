379d93ff38be56d261acbb1c1dc7043aa3aa304f
Grzegorz <grzegorz_sroda@wp.pl>
2024-08-14 16:55:47 +0200

    Additional validation annotations created. Now RequestBody of registeredUser requires All fields are not blank Email has to be correct format Password needs to be at least 8 characters but not more than 32 and 1 number,letter

--- src/main/java/pl/grzegorz/portfolio/ecommerce_project/api/model/RegistrationBody.java
+++ src/main/java/pl/grzegorz/portfolio/ecommerce_project/api/model/RegistrationBody.java
@@ -1,18 +1,36 @@
 package pl.grzegorz.portfolio.ecommerce_project.api.model;
 
-import org.hibernate.validator.constraints.Email;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @SuppressWarnings("ALL")
 public class RegistrationBody {
 
+    @NotBlank
+    @Size(min = 3, max = 255, message = "Username must be between 3 and 255 characters")
     private String username;
 
-    @Email
+    @Email(message = "Email should be valid")
+    @NotBlank
     private String email;
+
+    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+            message = "Password must be at least 8 characters long, contain at least one letter and one number."
+    )
+    @Size(min = 8, max = 32, message = "Password must be between 8 and 32 characters")
     private String password;
+
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
 
+    // Getters and Setters
     public String getUsername() {
         return username;
     }
