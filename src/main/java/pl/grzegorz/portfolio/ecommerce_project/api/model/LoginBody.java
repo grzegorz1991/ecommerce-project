d6c41ad44bdc1769424838505153a41e66cd17b1
Grzegorz <grzegorz_sroda@wp.pl>
2024-08-16 01:01:33 +0200

    @NotBlank annotation added to fields username and password.

--- src/main/java/pl/grzegorz/portfolio/ecommerce_project/api/model/LoginBody.java
+++ src/main/java/pl/grzegorz/portfolio/ecommerce_project/api/model/LoginBody.java
@@ -1,9 +1,12 @@
 package pl.grzegorz.portfolio.ecommerce_project.api.model;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginBody {
 
+    @NotBlank
     private String username;
-
+    @NotBlank
     private String password;
 
     public String getUsername() {
